65255da7518ee5d781be474e725880093741f794
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2025-12-31 22:39:16 +0500

    arry

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-console.log("Hello world");
+// console.log("Hello world");
 
-let a=5;
-let b=20;
-let mins=b-a;
-console.log(mins);
+// let a=5;
+// let b=20;
+// let mins=b-a;
+// console.log(mins);
+// let a =29;
+// let b =19;
+// if(a>b){
+//     console.log("True");}
+//     else{"Fales"};
+let myArray=[2,33,44,55];
+for(let i=0; i<myArray.length; i++){
+    console.log(myArray[i]);
+}
