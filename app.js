ecedebc37f07f6eff12f09cbe22a2d324b033e1e
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2025-12-31 23:27:18 +0500

    ues console dot table

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ let myArray=[2,33,44,55];
 for(let i=0; i<myArray.length; i++){
     console.log(myArray[i]);
 }
+console.log("hello");// let myArray=[2,33,44,55];
+// for(let i=0; i<myArray.length; i++){
+//     console.log(myArray[i]);
+// }
+/*
+Paragraphs comments star comment and comment star
+*/
+const firstName="Ahmed Malik";
+let secandName="Ali";
+var lastName="Muntazim";
+// firstName="Hamza";
+secandName="Haris";
+lastName="Munib";
+console.table([firstName, secandName, lastName]);
+// I learn new console method console.table this table methat show show data in table format  value add in just like array 
+// var ko redefine kar saktehain or redeclear bhi or ya hositable bhi hota hai or ye function scpe he
+// const ko redefien redclear nahi kar sakhte hen or ye block scope he or ye hositable nahi hota he 
